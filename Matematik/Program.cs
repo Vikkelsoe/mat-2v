8f1513d4b342bf1d7e1006755228a8d17f15584a
Christian Vikkelsø <chri21i5@edu.nextkbh.dk>
2019-12-06 15:05:13 +0100

    Opdatering af 2D vektorer

--- Matematik/Program.cs
+++ Matematik/Program.cs
@@ -7,6 +7,18 @@ namespace Matematik
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Punkt2D p1 = new Punkt2D(0, 0);
+            Punkt2D p2 = new Punkt2D(1, 1);
+            Punkt2D p3 = new Punkt2D(5, 8);
+            Punkt2D p4 = new Punkt2D(4, 1);
+
+            Vektor2D v1 = new Vektor2D(p1,p2);
+            Vektor2D v2 = new Vektor2D(p3,p4);
+
+            Console.WriteLine(v1.BeregnLængde());
+            Console.WriteLine(v1.BeregnSkalarprodukt(v2));
+    
+
         }
     }
 }
---
+++ Matematik/Vektor2D.cs
@@ -0,0 +1,64 @@
+using Matematik;
+using System;
+
+class Vektor2D : IVektor2D
+{
+    private const double V = 0.5;
+
+    public IPunkt2D Punkt { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public IPunkt2D Startpunkt { get; set; }
+    public IPunkt2D Slutpunkt { get; set; }
+    public double Længde { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public Vektor2D(IPunkt2D startpunkt, IPunkt2D slutpunkt)
+    {
+        Startpunkt = startpunkt;
+        Slutpunkt = slutpunkt;
+
+    }
+
+
+
+    public double BeregnLængde()
+    {
+        double xKomposant = Slutpunkt.X - Startpunkt.X;
+        double yKomposant = Slutpunkt.Y - Startpunkt.Y;
+
+        return Math.Pow(Math.Pow(xKomposant,2) + Math.Pow(yKomposant,2), 0.5);
+    }
+
+    public double BeregnSkalarprodukt(IVektor2D vektor2)
+    {
+        double v1xKomposant = Slutpunkt.X - Startpunkt.X;
+        double v1yKomposant = Slutpunkt.Y - Startpunkt.Y;
+        double v2xKomposant = vektor2.Slutpunkt.X - vektor2.Startpunkt.X;
+        double v2yKomposant = vektor2.Slutpunkt.Y - vektor2.Startpunkt.Y;
+        
+        return (v1xKomposant*v2xKomposant + v1yKomposant*v2yKomposant);
+    }
+
+    public IVektor2D BeregnTværvektor()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IVektor2D BeregnVektorsum(IVektor2D vektor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public double BeregnVinkelMedXAkse()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool ErOrtogonalMed(IVektor2D vektor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool ErParallelMed(IVektor2D vektor)
+    {
+        throw new System.NotImplementedException();
+    }
+}
